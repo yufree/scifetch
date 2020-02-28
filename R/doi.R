928249842827725adcd650f0cff5aa7ce3e4438c
yufree <yufree@live.cn>
2020-02-28 10:45:26 -0500

    update

--- R/doi.R
+++ R/doi.R
@@ -5,14 +5,14 @@
 #' @export
 doiref <- function(path, style = 'apa'){
         mystring <- readLines(path)
-        doi <- grep("\\[\\@\\b10\\.(\\d+\\.*)+[\\/](([^\\s\\.])+\\.*)+\\b\\]", mystring, value=TRUE)
+        doi <- unlist(stringr::str_extract_all(mystring,"\\[\\@\\b10\\.(\\d+\\.*)+[\\/](([^\\s\\.])+\\.*)+\\b\\]"))
         doi <- unique(doi)
         n <- as.numeric(factor(doi,levels = doi))
-        x <- mystring
         for (i in 1:length(doi)){
-                temp <- try(rcrossref::cr_cn(dois = unlist(strsplit(doi[i],split = '\\^|\\]'))[2], format = "text", style = style), T)
-                cat(temp,file=path,sep="\n",append=TRUE)
-                x <- suppressWarnings(gsub(doi[i], paste0('[',n[i],']'),x,fixed = T))
+                x <- readLines(path)
+                x <- gsub(doi[i], paste0('[',n[i],']'),x,fixed = T)
                 writeLines(x,path)
+                temp <- try(rcrossref::cr_cn(dois = unlist(strsplit(doi[i],split = '\\[@|\\]'))[2], format = "text", style = style), T)
+                cat(temp,file=path,sep="\n",append=TRUE)
         }
 }
